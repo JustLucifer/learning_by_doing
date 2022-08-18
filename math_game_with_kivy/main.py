bebd69fe2997c9a01ae506a0a71ab7e29b1e6ee7
JustLucifer <my.names_is_no_one@protonmail.ch>
2022-08-18 14:08:13 +0300

    Add check for answer

--- math_game_with_kivy/main.py
+++ math_game_with_kivy/main.py
@@ -1,3 +1,4 @@
+# python -m pip install "kivy[base]" kivy_examples
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
@@ -16,20 +17,20 @@ class MainWindow(Widget):
         return str(self.x) + ' ' + self.oper + ' ' + str(self.y)
         
     def check_answer(self):
-        # self.task = ObjectProperty(None)
-        
         res = calculation(self.x, self.oper, self.y)
-        
-        if int(self.answer.text) == res:
-            self.ids.result.text = 'Right!'
-            self.result.background_color = 0, 0.8, 0, 1
-            print("Right!")
+        try:
+            answer = int(self.answer.text)
+        except ValueError:
+            pass
         else:
-            self.result.background_color = 1, 0, 0, 1
-            self.ids.result.text = 'Wrong!'
-            print("Wrong!")
+            if answer == res:
+                self.ids.result.text = 'Right!'
+                self.result.background_color = 0, 0.8, 0, 1
+            else:
+                self.result.background_color = 1, 0, 0, 1
+                self.ids.result.text = 'Wrong!'
         
-        self.ids.task.text = self.generate_task()
+            self.ids.task.text = self.generate_task()
         
         self.answer.text = ''
 
