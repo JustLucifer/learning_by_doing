8d59a4b96516aa24991eec1e56fffaf7c762c19b
JustLucifer <my.names_is_no_one@protonmail.ch>
2022-09-19 13:59:39 +0300

    Rewrite GUI model

--- pySimpleGUI/simpleCalculatorGUI/GUI_calculator_model.py
+++ pySimpleGUI/simpleCalculatorGUI/GUI_calculator_model.py
@@ -1,35 +1,34 @@
 import PySimpleGUI as sg
+from PySimpleGUI import B
 
 sg.theme('DarkBrown4')
 
 col = [
-    [sg.B('7', size=(3,2), font='bold'), sg.B('8', size=(3,2), font='bold'), sg.B('9', size=(3,2), font='bold')],
-    [sg.B('4', size=(3,2), font='bold'), sg.B('5', size=(3,2), font='bold'), sg.B('6', size=(3,2), font='bold')],
-    [sg.B('1', size=(3,2), font='bold'), sg.B('2', size=(3,2), font='bold'), sg.B('3', size=(3,2), font='bold')],
-    [sg.B(u'\u221A', size=(3,2), font='bold', key='-SQRT-'), sg.B('0', size=(3,2), font='bold'), 
-     sg.B('.', size=(3,2), font='bold', key='-DOT-')],
+    [B('7', s=(3,2)), B('8', s=(3,2)), B('9', s=(3,2))],
+    [B('4', s=(3,2)), B('5', s=(3,2)), B('6', s=(3,2))],
+    [B('1', s=(3,2)), B('2', s=(3,2)), B('3', s=(3,2))],
+    [B(u'\u221A', s=(3,2), k='-SQRT-'), B('0', s=(3,2)), B('.', s=(3,2), k='-DOT-')],
 ]
 
 col_1 = [
-    [sg.B('*', size=(3,2), font='bold', key='*')],
-    [sg.B('-', size=(3,2), font='bold', key='-')],
-    [sg.B('+', size=(3,5), font='bold', key='+')],
+    [B('*', s=(3,2), k='*')],
+    [B('-', s=(3,2), k='-')],
+    [B('+', s=(3,5), k='+')],
 ]
 
 col_2 = [
-    [sg.B('/', size=(3,2), font='bold', key='/')],
-    [sg.B('C', size=(3,2), font='bold', key='-CLEAR-')],
-    [sg.B('CE', size=(3,2), font='bold', key='-CLEAR_EVR-')],
-    [sg.B('=', size=(3,2), font='bold', key='-EQUAL-',
-          button_color='#b30059')],
+    [B('/', s=(3,2), k='/')],
+    [B('C', s=(3,2), k='-CLEAR-')],
+    [B('CE', s=(3,2), k='-CLEAR_EVR-')],
+    [B('=', s=(3,2), k='-EQUAL-', button_color='#b30059')],
 ]
 
 layout = [
-    [sg.I(font=(None, 28), size=(14,1), key='-INPUT-',
+    [sg.I(font=(None, 28), s=(14,1), k='-INPUT-',
           background_color='#ffb380', text_color='#000000',)],
-    [sg.T(key='-ERROR_OUT-', size=(None,1), pad=0, text_color='#faf9f3')],
-    [sg.Col(col, pad=0), sg.VerticalSeparator(color='#ffb380'),
-     sg.Col(col_1, pad=0), sg.Col(col_2, pad=0)],
+    [sg.T(k='-ERROR_OUT-', s=(None,1), p=0, text_color='#faf9f3')],
+    [sg.Col(col, p=0), sg.VerticalSeparator(color='#ffb380'),
+     sg.Col(col_1, p=0), sg.Col(col_2, p=0)],
 ]
 
-window = sg.Window('Simple Calculator', layout)
+window = sg.Window('Simple Calculator', layout, font='bold')
