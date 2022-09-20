f5018d43901c92ad28b01ec2e4548beea4bd5636
JustLucifer <my.names_is_no_one@protonmail.ch>
2022-09-20 14:11:56 +0300

    Fixed sqrt button

--- pySimpleGUI/simpleCalculatorGUI/simpleCalculator.py
+++ pySimpleGUI/simpleCalculatorGUI/simpleCalculator.py
@@ -15,55 +15,61 @@ class SimpleCalculator:
     
     def calc_res_of_expression(self):
         val = self.history.rstrip('+-*/')
+
         for n, i in enumerate(val):
             if i in self.OPERATORS and n != 0:
                 oper = i
                 x = val[:n]
                 y = val[n + 1:]
+
         if int(y) == 0 and oper == '/':
             window['-ERROR_OUT-']("Can't divide by zero")
         else:
             self.history = str(calc(float(x), oper, float(y)))
+    
+    def pre_check_history(self, next_func):
+        if re.match('[*/+-]$', self.history) \
+        or re.search('[*/+-]{2,}', self.history) \
+        or re.match('[*/+-]?[.]', self.history) \
+        or re.match('((\d*[.])?\d+)?[*/+-][.]', self.history) \
+        or re.match('[*/](\d*[.])?\d+[*/+-]?((\d*[.])?\d+[*/+-]?)*$', self.history):
+            window['-ERROR_OUT-'](MalformedExpressionError())
+        else:
+            if next_func == '=':
+                self.equal()
+            else:
+                self.calc_square_root()
 
     def equal(self):
-        if self.history == '' or \
-        re.match('[*/+-]$', self.history):
-            return
+        if re.match('[+-]?(\d*[.])?\d+[*/+-](\d*[.])?\d+[*/+-](\d*[.])?\d+', self.history):
+            window['-ERROR_OUT-'](CantDoTwoOperationsError())
+        elif re.match('[+-]?(\d*[.])?\d+[*/+-]?$', self.history):
+            self.history = str(float(self.history.strip('+-*/')) * 2)
         else:
-            if re.search('[*/+-]{2,}', self.history) \
-            or re.match('[*/+-]?[.]', self.history) \
-            or re.match('((\d*[.])?\d+)?[*/+-][.]', self.history) \
-            or re.match('[*/]?(\d*[.])?\d+[*/+-]$', self.history) \
-            or re.match('[*/](\d*[.])?\d+[*/+-]?((\d*[.])?\d+[*/+-]?)*$', self.history):
-                window['-ERROR_OUT-'](MalformedExpressionError())
-            elif re.match('[+-]?(\d*[.])?\d+[*/+-](\d*[.])?\d+[*/+-](\d*[.])?\d+', self.history):
-                window['-ERROR_OUT-'](CantDoTwoOperationsError())
-            elif re.match('^[+-]?(\d*[.])?\d+$', self.history):
-                self.history = str(float(self.history) * 2)
-            else:
-                self.calc_res_of_expression()
-
-    def clear_input_field(self, param='C'):
-        if param == 'C':
-            self.history = self.history[:-1]
-        else:
-            self.history = ''
+            self.calc_res_of_expression()
     
     def calc_square_root(self):
+
         if self.history[0] == '-' or self.history[-1] == 'i':
             minus = 'i'
         else:
             minus = ''
 
+        # check position of operator
         for n, i in enumerate(self.history):
-            if i in '+-*/' and n != len(self.history) - 1 \
-                and n != 0:
+            if i in '+-*/' and n != len(self.history) - 1 and n != 0:
                 window['-ERROR_OUT-'](MalformedExpressionError())
                 return
 
         val = self.history.strip('+-*/i')
         self.history = str(round(sqrt(float(val)), 6)) + minus
-    
+
+    def clear_input_field(self, param):
+        if param == 'C':
+            self.history = self.history[:-1]
+        else:
+            self.history = ''
+
     def change_history(self, event):
         if event == sg.WIN_CLOSED:
                 window.close()
@@ -77,15 +83,10 @@ class SimpleCalculator:
         elif self.history == '':
             return
         else:
-            match event:
-                case '=':
-                    self.equal()
-                case 'C':
-                    self.clear_input_field()
-                case 'CE':
-                    self.clear_input_field(event)
-                case '-SQRT-':
-                    self.calc_square_root()
+            if event.startswith('C'):
+                self.clear_input_field(event)
+            else:
+                self.pre_check_history(event)
 
     def run(self):
         while True:
