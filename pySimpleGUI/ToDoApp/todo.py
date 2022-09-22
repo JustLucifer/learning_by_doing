d15c6e597704bb40cd89b8454946c422380c9f4d
JustLucifer <my.names_is_no_one@protonmail.ch>
2022-09-22 08:13:00 +0300

    Implemented add (task) function

--- pySimpleGUI/ToDoApp/todo.py
+++ pySimpleGUI/ToDoApp/todo.py
@@ -3,15 +3,33 @@ import sys
 
 class ToDo:
     
+    def add_task(self):
+        layout2 = [
+            [sg.I(s=(20,1), font='bold', k='-TASK-')],
+            [sg.CalendarButton('Set date'), sg.T('', k='-DATE-')],
+            [sg.Ok(), sg.Cancel()],
+        ]
+
+        window2 = sg.Window('Add task', layout2)
+
+        while True:
+            event2, values2 = window2.read()
+            if event2 == 'Ok':
+                date = window2['-DATE-'].get()
+                tasks.append([date, values2['-TASK-']])
+                window2.close()
+                window['-TABLE-'](tasks)
+                break
+            elif event2 in ('Cancel', sg.WIN_CLOSED):
+                window2.close()
+                break
+
     def run(self):
         while True:
             event, values = window.read()
             print(event, values)
-            if event in ('Exit', sg.WINDOW_CLOSED):
+            if event in ('Exit', sg.WIN_CLOSED):
                 window.close()
                 sys.exit()
-            elif event.split(':')[0] == 'Delete':
-                if values['-TABLE-']:
-                    index = values['-TABLE-'][0]
-                    del tasks[index]
-                    window['-TABLE-'](tasks)
+            elif event == 'Add':
+                self.add_task()
--- pySimpleGUI/ToDoApp/todo_gui_model.py
+++ pySimpleGUI/ToDoApp/todo_gui_model.py
@@ -1,25 +1,17 @@
 import PySimpleGUI as sg
 
-tasks_1 = [['2022-09-22', 'buy milk'], ['2022-09-22', 'buy shoes'], ['2022-09-23', 'feed cat']]
-tasks = [x[1:] for x in tasks_1]
-today_tab = [
-    [sg.Table(values=tasks, headings=['Tasks'], k='-TABLE-',
-              num_rows=21, enable_events=True, auto_size_columns=False,
-              hide_vertical_scroll=True, row_height=30,
-              col_widths=[40], justification='l', font='None 15')],
-]
+tasks = [['2022-09-22', 'buy milk'], ['2022-09-22', 'buy shoes'], ['2022-09-23', 'feed cat']]
 
-inbox_tab = [
-    [sg.Table(values=tasks_1, headings=['Date', 'Tasks'], k='-TABLE-',
+frame = [
+    [sg.Table(values=tasks, headings=['Date', 'Tasks'], k='-TABLE-',
               num_rows=21, enable_events=True, auto_size_columns=False,
               hide_vertical_scroll=True, row_height=30,
               col_widths=[10,30], justification='l', font='None 15')],
 ]
 layout = [
-    [sg.TabGroup([
-        [sg.Tab('Today', today_tab)], [sg.Tab('Inbox', inbox_tab)]])],
-    [sg.B('Add', s=(5,1)),sg.T('   '), sg.B('Delete')]
+    [sg.Frame('To Do', frame)],
+    [sg.B('Add', s=(5,1)), sg.T('   '), sg.B('Delete')]
 ]
 
 window = sg.Window('ToDo', layout, finalize=True, element_justification='c',
-                   font='bold', size=(550, 750), return_keyboard_events=True)
+                   font='bold', size=(525, 750), return_keyboard_events=True)
