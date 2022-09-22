474a7bdd85eaacd219e6db7e91da01eb15cf58b4
JustLucifer <my.names_is_no_one@protonmail.ch>
2022-09-22 08:50:00 +0300

    Implemented delete function

--- pySimpleGUI/ToDoApp/todo.py
+++ pySimpleGUI/ToDoApp/todo.py
@@ -1,25 +1,33 @@
 from todo_gui_model import sg, window, tasks
+from datetime import datetime
 import sys
 
 class ToDo:
+
+    def get_today_date(self):
+        today_date = datetime.today()
+        today_date_str = today_date.strftime("%d-%m-%Y")
+        return today_date_str
+
     
     def add_task(self):
         layout2 = [
-            [sg.I(s=(20,1), font='bold', k='-TASK-')],
-            [sg.CalendarButton('Set date'), sg.T('', k='-DATE-')],
-            [sg.Ok(), sg.Cancel()],
+            [sg.I(s=(15,2), font=(None,25), k='-TASK-')],
+            [sg.CalendarButton('Set date', font='bold'), sg.T(self.get_today_date(), font='bold', k='-DATE-')],
+            [sg.Ok(font='bold'), sg.Cancel(font='bold')],
         ]
 
-        window2 = sg.Window('Add task', layout2)
+        window2 = sg.Window('Add task', layout2, finalize=True)
 
         while True:
             event2, values2 = window2.read()
             if event2 == 'Ok':
-                date = window2['-DATE-'].get()
-                tasks.append([date, values2['-TASK-']])
-                window2.close()
-                window['-TABLE-'](tasks)
-                break
+                if values2['-TASK-']:
+                    date = window2['-DATE-'].get()
+                    tasks.append([date, values2['-TASK-']])
+                    window2.close()
+                    window['-TABLE-'](tasks)
+                    break
             elif event2 in ('Cancel', sg.WIN_CLOSED):
                 window2.close()
                 break
@@ -33,3 +41,8 @@ class ToDo:
                 sys.exit()
             elif event == 'Add':
                 self.add_task()
+            elif event == 'Delete':
+                if values['-TABLE-']:
+                    index = values['-TABLE-'][0]
+                    del tasks[index]
+                    window['-TABLE-'](tasks)
