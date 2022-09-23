2a1eef440e211f5dd3c95bd83999dff70789a386
JustLucifer <my.names_is_no_one@protonmail.ch>
2022-09-23 19:47:15 +0300

    Created UI for tic-tac-toe

--- pySimpleGUI/SimpleTicTacToe/tic_tac_toe.py
+++ pySimpleGUI/SimpleTicTacToe/tic_tac_toe.py
@@ -1,24 +1,29 @@
 import PySimpleGUI as sg
 
-sg.theme('Reddit')
+sg.theme('LightBlue')
 
 class TicTacToe:
     
-    def __init__(self) -> None:
-        layout = [[sg.T('Player 1', font='Arial 15'), sg.T(text=' ' * 47), sg.T('Player 2', font='Arial 15'),],
-                  [sg.T(text=' ' * 3), sg.T(0, font='bold'), sg.T(text=' ' * 63), sg.T(0, font='bold')],
-                  [[sg.B(' ', s=(10,8), font='bold', p=0, k=f'{i+1} {j+1}') for j in range(3)] for i in range(3)]]
-        self.window = sg.Window('TicTacToe', layout)
-        self.turn = 'first'
-        self.cells = self.create_cells_for_check()
+    NUMS = [i for i in range(9)]
     
-    def create_cells_for_check(self) -> list:
-        cells = []
-        for i in range(3):
-            for j in range(3):
-                cells.append(f'{i+1} {j+1}')
-        return cells
-
+    def __init__(self) -> None:
+        self.turn = 'first'
+        self.cells = ['' for _ in range(9)]
+        self.layout = [[sg.T('Player 1', font='Arial 15'), sg.T(text=' ' * 47), sg.T('Player 2', font='Arial 15'),],
+                  [sg.T(text=' ' * 3), sg.T(0, font='bold'), sg.T(text=' ' * 63), sg.T(0, font='bold')],
+                  [sg.B(self.cells[0], s=(10,8), font='bold', p=0, k='0', border_width=3), 
+                   sg.B(self.cells[1], s=(10,8), font='bold', p=0, k='1', border_width=3),
+                   sg.B(self.cells[2], s=(10,8), font='bold', p=0, k='2', border_width=3)],
+                  [sg.B(self.cells[3], s=(10,8), font='bold', p=0, k='3', border_width=3), 
+                   sg.B(self.cells[4], s=(10,8), font='bold', p=0, k='4', border_width=3),
+                   sg.B(self.cells[5], s=(10,8), font='bold', p=0, k='5', border_width=3)],
+                  [sg.B(self.cells[6], s=(10,8), font='bold', p=0, k='6', border_width=3), 
+                   sg.B(self.cells[7], s=(10,8), font='bold', p=0, k='7', border_width=3),
+                   sg.B(self.cells[8], s=(10,8), font='bold', p=0, k='8', border_width=3)]
+        ]
+        
+        self.window = sg.Window('TicTacToe', self.layout)
+    
     def run(self) -> None:
         while True:
             event, values = self.window.read()
@@ -26,10 +31,13 @@ class TicTacToe:
             if event == sg.WIN_CLOSED:
                 self.window.close()
                 break
-            elif event in self.cells:
-                if self.turn == 'first':
-                    self.window[event]('X')
-                    self.turn = 'second'
-                elif self.turn == 'second':
-                    self.window[event]('O')
-                    self.turn = 'first'
+            elif int(event) in self.NUMS:
+                if self.cells[int(event)] == '':
+                    if self.turn == 'first':
+                        self.cells[int(event)] = 'X'
+                        self.turn = 'second'
+                        self.window[event]('X')
+                    elif self.turn == 'second':
+                        self.cells[int(event)] = 'O'
+                        self.window[event]('O')
+                        self.turn = 'first'
