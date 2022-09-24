1e5246bbdd73e4600153ed1b34ad077cb4c1fb64
JustLucifer <my.names_is_no_one@protonmail.ch>
2022-09-24 08:53:46 +0300

    Created x's and o's buttons

--- pySimpleGUI/SimpleTicTacToe/tic_tac_toe.py
+++ pySimpleGUI/SimpleTicTacToe/tic_tac_toe.py
@@ -1,6 +1,6 @@
 import PySimpleGUI as sg
 
-sg.theme('LightBlue')
+sg.theme('LightGrey3')
 
 class TicTacToe:
     
@@ -9,20 +9,30 @@ class TicTacToe:
     def __init__(self) -> None:
         self.turn = 'first'
         self.cells = ['' for _ in range(9)]
-        self.layout = [[sg.T('Player 1', font='Arial 15'), sg.T(text=' ' * 47), sg.T('Player 2', font='Arial 15'),],
-                  [sg.T(text=' ' * 3), sg.T(0, font='bold'), sg.T(text=' ' * 63), sg.T(0, font='bold')],
-                  [sg.B(self.cells[0], s=(10,8), font='bold', p=0, k='0', border_width=3), 
-                   sg.B(self.cells[1], s=(10,8), font='bold', p=0, k='1', border_width=3),
-                   sg.B(self.cells[2], s=(10,8), font='bold', p=0, k='2', border_width=3)],
-                  [sg.B(self.cells[3], s=(10,8), font='bold', p=0, k='3', border_width=3), 
-                   sg.B(self.cells[4], s=(10,8), font='bold', p=0, k='4', border_width=3),
-                   sg.B(self.cells[5], s=(10,8), font='bold', p=0, k='5', border_width=3)],
-                  [sg.B(self.cells[6], s=(10,8), font='bold', p=0, k='6', border_width=3), 
-                   sg.B(self.cells[7], s=(10,8), font='bold', p=0, k='7', border_width=3),
-                   sg.B(self.cells[8], s=(10,8), font='bold', p=0, k='8', border_width=3)]
+        self.layout = [[sg.T('Player 1', font='Arial 15', text_color='black'), sg.T(text=' ' * 38), sg.T('Player 2', font='Arial 15', text_color='black'),],
+                  [sg.T(text=' ' * 3), sg.T(0, font='bold', text_color='#77d6ec'), sg.T(text=' ' * 54), sg.T(0, font='bold', text_color='#eda4a4')],
+                  [sg.B(self.cells[0], font='bold', p=0, k='0', border_width=3, 
+                   image_filename='pySimpleGUI/SimpleTicTacToe/images/white.png'), 
+                   sg.B(self.cells[1], font='bold', p=0, k='1', border_width=3, 
+                   image_filename='pySimpleGUI/SimpleTicTacToe/images/white.png'),
+                   sg.B(self.cells[2], font='bold', p=0, k='2', border_width=3, 
+                   image_filename='pySimpleGUI/SimpleTicTacToe/images/white.png')],
+                  [sg.B(self.cells[3], font='bold', p=0, k='3', border_width=3, 
+                   image_filename='pySimpleGUI/SimpleTicTacToe/images/white.png'), 
+                   sg.B(self.cells[4], font='bold', p=0, k='4', border_width=3, 
+                   image_filename='pySimpleGUI/SimpleTicTacToe/images/white.png'),
+                   sg.B(self.cells[5], font='bold', p=0, k='5', border_width=3, 
+                   image_filename='pySimpleGUI/SimpleTicTacToe/images/white.png')],
+                  [sg.B(self.cells[6], font='bold', p=0, k='6', border_width=3, 
+                   image_filename='pySimpleGUI/SimpleTicTacToe/images/white.png'), 
+                   sg.B(self.cells[7], font='bold', p=0, k='7', border_width=3, 
+                   image_filename='pySimpleGUI/SimpleTicTacToe/images/white.png'),
+                   sg.B(self.cells[8], font='bold', p=0, k='8', border_width=3, 
+                   image_filename='pySimpleGUI/SimpleTicTacToe/images/white.png')]
         ]
         
-        self.window = sg.Window('TicTacToe', self.layout)
+        self.window = sg.Window('TicTacToe', self.layout, default_button_element_size=(105,105),
+                                size=(360,410))
     
     def run(self) -> None:
         while True:
@@ -36,8 +46,8 @@ class TicTacToe:
                     if self.turn == 'first':
                         self.cells[int(event)] = 'X'
                         self.turn = 'second'
-                        self.window[event]('X')
+                        self.window[event](image_filename='pySimpleGUI/SimpleTicTacToe/images/x.png')
                     elif self.turn == 'second':
                         self.cells[int(event)] = 'O'
-                        self.window[event]('O')
+                        self.window[event](image_filename='pySimpleGUI/SimpleTicTacToe/images/o.png')
                         self.turn = 'first'
