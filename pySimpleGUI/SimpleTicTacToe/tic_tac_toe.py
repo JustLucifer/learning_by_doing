1aeb539a239b1a8de4679867ca58622ab18c203d
JustLucifer <my.names_is_no_one@protonmail.ch>
2022-09-25 08:36:29 +0300

    Code refactoring (tictactoe buttons)

--- pySimpleGUI/SimpleTicTacToe/tic_tac_toe.py
+++ pySimpleGUI/SimpleTicTacToe/tic_tac_toe.py
@@ -3,7 +3,7 @@ import PySimpleGUI as sg
 
 class TicTacToe:
     
-    NUMS = [i for i in range(9)]
+    NUMS = [0] + [f'0{i}' for i in range(8)]
     x_image = 'pySimpleGUI/SimpleTicTacToe/images/x.png'
     o_image = 'pySimpleGUI/SimpleTicTacToe/images/o.png'
     sg.theme('LightGrey3')
@@ -16,16 +16,8 @@ class TicTacToe:
         self.turn = 'Player1'
         self.cells = ['' for _ in range(9)]
         self.layout = [[sg.T('Player 1', font='Arial 15', p=2), sg.T(text=' ' * 38), sg.T('Player 2', font='Arial 15', p=2),],
-                  [sg.T(0, font='bold', text_color='#77d6ec', p=2), sg.T(text=' ' * 54), sg.T(0, font='bold', text_color='#eda4a4', p=2)],
-                  [sg.B('', k=0, f='bold', mouseover_colors='#f1ede3', image_filename=button_def_image),
-                   sg.B('', k=1, f='bold', mouseover_colors='#f1ede3', image_filename=button_def_image),
-                   sg.B('', k=2, f='bold', mouseover_colors='#f1ede3', image_filename=button_def_image)],
-                  [sg.B('', k=3, f='bold', mouseover_colors='#f1ede3', image_filename=button_def_image),
-                   sg.B('', k=4, f='bold', mouseover_colors='#f1ede3', image_filename=button_def_image),
-                   sg.B('', k=5, f='bold', mouseover_colors='#f1ede3', image_filename=button_def_image)],
-                  [sg.B('', k=6, f='bold', mouseover_colors='#f1ede3', image_filename=button_def_image),
-                   sg.B('', k=7, f='bold', mouseover_colors='#f1ede3', image_filename=button_def_image),
-                   sg.B('', k=8, f='bold', mouseover_colors='#f1ede3', image_filename=button_def_image)]
+                       [sg.T(0, font='bold', text_color='#77d6ec', p=2), sg.T(text=' ' * 54), sg.T(0, font='bold', text_color='#eda4a4', p=2)],
+                       [[sg.B('', k=0, f='bold', mouseover_colors='#f1ede3', image_filename=button_def_image) for _ in range(3)] for _ in range(3)]
         ]
 
         self.window = sg.Window('TicTacToe', self.layout, size=(360,400),
@@ -38,13 +30,14 @@ class TicTacToe:
             if event == sg.WIN_CLOSED:
                 self.window.close()
                 break
-            elif int(event) in self.NUMS:
-                if self.cells[int(event)] == '':
+            elif event in self.NUMS:
+                index = self.NUMS.index(event)
+                if self.cells[index] == '':
                     if self.turn == 'Player1':
-                        self.cells[int(event)] = 'X'
+                        self.cells[index] = 'X'
                         self.turn = 'Player2'
                         self.window[event](image_filename=self.x_image)
                     elif self.turn == 'Player2':
-                        self.cells[int(event)] = 'O'
+                        self.cells[index] = 'O'
                         self.window[event](image_filename=self.o_image)
                         self.turn = 'Player1'
